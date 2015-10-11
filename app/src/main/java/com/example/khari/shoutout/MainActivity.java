ed51fa51d286ff8405d83a66f3df81f744aff746
Khari <kseals281@gmail.com>
2015-10-11 05:17:49 -0400

    updating to where it shows login page but doesn't login

---
+++ app/src/main/java/com/example/khari/shoutout/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.khari.shoutout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+//    public void login(View v) {
+//        Intent myIntent = new Intent(this, ListingActivity.class);
+//        this.startActivity(myIntent);
+//}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
